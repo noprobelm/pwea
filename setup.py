78d3bc8a8351559c733b2647e09893d67b441a2b
Jeff Barfield <jeffreybarfield@protonmail.com>
2021-10-23 14:50:36 -0400

    Version 1.0 commit

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 import pathlib
 from setuptools import setup
 
-
-__version__ = '0.3'
+__version__ = '1.0'
 __author__ = 'Jeff Barfield'
-__author_email__ = 'jeffreybarfield@protonmail.com'
-
+__author_email__ = 'noprobelm@protonmail.com'
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / 'README.md').read_text()
 
-
 setup(
     name='pwea',
     version=__version__,
-    description='A simple weather tool.',
+    description='A simple tool for displaying weather data in the command line.',
     long_description=README,
     long_description_content_type='text/markdown',
-    url='https://gitlab.com/jeffreybarfield/myweather',
+    url='https://github.com/noprobelm/pwea',
     author=__author__,
     author_email=__author_email__,
     license='MIT',
@@ -28,7 +25,7 @@ setup(
     ],
     packages=['pwea'],
     include_package_data=True,
-    install_requires=['requests', 'rich'],
+    install_requires=['requests', 'rich', 'argparse'],
     entry_points={'console_scripts': ['pwea=pwea.__main__:main']},
     python_requires=">=3.6"
 )
